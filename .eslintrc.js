5da28197470ae1163c97724df19a402579cea793
Pablo F. Mescher <pfmescher@gmail.com>
2022-09-30 11:59:05 -0300

    Switched project to use eslint instead of tslint

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    browser: false,
+    node: true
+  },
+  extends: 'standard-with-typescript',
+  overrides: [
+  ],
+  parserOptions: {
+    project: 'tsconfig.json',
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+  }
+}
