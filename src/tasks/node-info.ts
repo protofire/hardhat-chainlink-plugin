34c7027fd12cdb28fcf03e2593af057d0d23ff9d
Pablo F. Mescher <pfmescher@gmail.com>
2022-10-31 13:53:26 -0300

    Updated node info query

--- src/tasks/node-info.ts
+++ src/tasks/node-info.ts
@@ -13,6 +13,10 @@ declare interface ResponseData {
       }>
     }
   }
+  errors?: Array<{
+    message: string
+    locations: any[]
+  }>
 }
 
 const getInfo = async (authToken: string): Promise<ResponseData> => {
@@ -23,7 +27,7 @@ const getInfo = async (authToken: string): Promise<ResponseData> => {
       cookie: `blocalauth=localapibe315fd0c14b5e47:; isNotIncognito=true; _ga=GA1.1.2055974768.1644792885; ${authToken}`,
       Referer: 'http://127.0.0.1:6688/jobs/new'
     },
-    data: '{"operationName":"FetchAccountBalances","variables":{},"query":"fragment AccountBalancesPayload_ResultsFields on EthKey {\\n  address\\n  chain {\\n    id\\n    __typename\\n  }\\n  ethBalance\\n  isFunding\\n  linkBalance\\n  __typename\\n}\\n\\nquery FetchAccountBalances {\\n  ethKeys {\\n    results {\\n      ...AccountBalancesPayload_ResultsFields\\n      __typename\\n    }\\n    __typename\\n  }\\n}\\n"}',
+    data: '{"operationName":"FetchAccountBalances","variables":{},"query":"fragment AccountBalancesPayload_ResultsFields on EthKey {\\n  address\\n  chain {\\n    id\\n    __typename\\n  }\\n  ethBalance\\n  linkBalance\\n  __typename\\n}\\n\\nquery FetchAccountBalances {\\n  ethKeys {\\n    results {\\n      ...AccountBalancesPayload_ResultsFields\\n      __typename\\n    }\\n    __typename\\n  }\\n}\\n"}',
     method: 'POST'
   })
 
@@ -35,12 +39,19 @@ export const nodeInfo = async (): Promise<void> => {
 
   try {
     const info = await getInfo(authenticationToken)
-
-    console.table({
-      Address: info.data?.ethKeys?.results[0]?.address,
-      Balance: info.data?.ethKeys?.results[0]?.ethBalance,
-      ChainID: info.data?.ethKeys?.results[0]?.chain?.id
-    })
+    if (info.errors != null) {
+      console.log('Errors found when trying to get node info:\n')
+      console.log(info.errors.reduce((acc, error) => {
+        acc += `\t${error.message}\n`
+        return acc
+      }, '\t'))
+    } else {
+      console.table({
+        Address: info.data?.ethKeys?.results[0]?.address,
+        Balance: info.data?.ethKeys?.results[0]?.ethBalance,
+        ChainID: info.data?.ethKeys?.results[0]?.chain?.id
+      })
+    }
   } catch (error) {
     console.log('Could not get Node address reason: ', error)
   }
